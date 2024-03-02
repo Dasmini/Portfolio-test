541ccaf30661c6498c3afe577281cf7ffcdf8933
dasMini <102203787+Dasmini@users.noreply.github.com>
2024-03-02 11:48:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text" , {
+    strings:["Frontend Developer","UI Designer","IOT Enthusiast"],
+    typeSpeed : 50,
+    backSpeed : 50,
+    backDelay : 1000,
+    loop : true
+});
